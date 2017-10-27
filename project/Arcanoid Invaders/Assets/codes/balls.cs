6f2cca08745d89cf315fb11f62b408f54cf9af01
ChaosTheLegend <chaosthelegend@inbox.ru>
2017-10-27 12:37:23 +0600

    added basic mechanics

---
+++ project/Arcanoid Invaders/Assets/codes/balls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class balls : MonoBehaviour {
+
+	public float force;
+	private float state;
+	public Transform folowingobj;
+	public float rotationangle;
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		state = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state == 0) {
+			transform.SetParent(folowingobj);
+			rb.isKinematic = true;
+
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				state = 1;
+			}
+		}
+		if (state == 1) {
+			rb.isKinematic = false;
+			transform.parent = null;
+			rb.AddForce (new Vector3 (force, force, 0f));
+			state = 2;
+		}
+		if (state == 2) {
+			transform.Rotate (Vector3.forward * Time.deltaTime * rotationangle);
+		}
+	}
+}
---
+++ project/Arcanoid Invaders/Assets/codes/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float speed;
+	public float maxtilt;
+	float moveHorizontal;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x + moveHorizontal * speed, -7.7f, 7.7f), -4.4f, 0f);
+		transform.rotation = Quaternion.Euler(0, 0, maxtilt * -moveHorizontal);
+	
+	}
+}
